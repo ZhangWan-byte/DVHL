c526bf91538463b0e5664e28f5ad8b52ec46a132
ZhangWan-byte <zhang_wanzhang@foxmail.com>
2023-10-03 21:26:55 +0800

    HumanModel

--- models/HumanModel.py
+++ models/HumanModel.py
@@ -4,6 +4,7 @@ class HumanModel(nn.Module):
     def __init__(self, cnn_type='resnet18'):
         super(HumanModel, self).__init__()
         
+        # cnn tower
         if cnn_type=="resnet18":
             self.cnn = resnet18()
         elif cnn_type=="resnet34":
@@ -11,3 +12,41 @@ class HumanModel(nn.Module):
         else:
             print("HumanModel wrong cnn_type!")
             exit()
+
+        # preference tower
+        self.mu = nn.Parameter(torch.zeros((16,1)))
+        self.logvar = nn.Parameter(torch.ones((16,1)))
+        self.user_weights = nn.Parameter(torch.rand((16,1)))
+    
+    def reparameterise(self, mu, logvar):
+        """sampling from gaussian distribution with mu and var
+
+        :param mu: mean of distribution
+        :param logvar: std of distribution - actually log version due to stability reason
+        :return: a sample from distribution
+        """
+        if self.training:
+            std = torch.exp(0.5 * logvar) # logvar.mul(0.5).exp_()
+            eps = torch.randn_like(std) # std.data.new(std.size()).normal_()
+            return eps * std + mu # eps.mul(std).add_(mu)
+        else:
+            return mu
+
+    def forward(self, x):
+
+        # cnn tower
+        visual_feature = self.cnn(x)
+
+        # preference tower
+        m = self.calc_metric(x)
+        d = self.reparameterise(self.mu, self.logvar)
+        w = F.sigmoid(self.user_weights)
+        user_preference = m * d * w
+
+        # prediction heads
+        q1 = self.q1(visual_feature, user_preference)
+        q2 = self.q1(visual_feature, user_preference)
+        q3 = self.q1(visual_feature, user_preference)
+        # ...
+
+        return q1, q2, q3
