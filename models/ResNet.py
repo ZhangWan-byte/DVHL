e556fea7da07a422247daa7fe6a6482253bbbe9d
ZhangWan-byte <zhang_wanzhang@foxmail.com>
2023-10-16 20:17:16 +0800

    mini resnet

--- models/ResNet.py
+++ models/ResNet.py
@@ -74,20 +74,20 @@ class ResNet(nn.Module):
     def __init__(self, block, num_block, num_classes=100):
         super().__init__()
 
-        self.in_channels = 64
+        self.in_channels = 16 # original: 64
 
         self.conv1 = nn.Sequential(
-            nn.Conv2d(10, 64, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(64),
+            nn.Conv2d(10, 16, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(16),
             nn.ReLU(inplace=True))
         #we use a different inputsize than the original paper
         #so conv2_x's stride is 1
-        self.conv2_x = self._make_layer(block, 64, num_block[0], 1)
-        self.conv3_x = self._make_layer(block, 128, num_block[1], 2)
-        self.conv4_x = self._make_layer(block, 256, num_block[2], 2)
-        self.conv5_x = self._make_layer(block, 512, num_block[3], 2)
+        self.conv2_x = self._make_layer(block, 16, num_block[0], 1)
+        self.conv3_x = self._make_layer(block, 32, num_block[1], 2)
+        self.conv4_x = self._make_layer(block, 64, num_block[2], 2)
+        self.conv5_x = self._make_layer(block, 128, num_block[3], 2)
         self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
-        self.fc = nn.Linear(512 * block.expansion, num_classes)
+        self.fc = nn.Linear(128 * block.expansion, num_classes)
 
     def _make_layer(self, block, out_channels, num_blocks, stride):
         """make resnet layers(by layer i didnt mean this 'layer' was the
@@ -125,6 +125,11 @@ class ResNet(nn.Module):
         output = self.fc(output)
 
         return output
+
+def resnet_mini():
+    """return a mini ResNet with 321,444 parameters
+    """
+    return ResNet(BasicBlock, [1, 1, 1, 1])
 
 def resnet18():
     """ return a ResNet 18 object
